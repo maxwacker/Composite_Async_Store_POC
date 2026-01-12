12e83143153d9ed76846505c5f4d588eabd96e7d
Maxime Wacker <maxime.wacker@free.fr>
2026-01-12 16:09:00 +0100

    Using A single mocking object for both Interacting and Presenting in CounterFeature SwiftUI Preview

--- LocalSPMS/CounterFeature/Sources/CounterView/CounterView.swift
+++ LocalSPMS/CounterFeature/Sources/CounterView/CounterView.swift
@@ -55,34 +55,14 @@ public struct CounterView: View {
 #if DEBUG
 import SwiftUI
 
-// Type-erased presenter wrapper for previews
+// MARK: - Mock Store for Previews
+
+/// A simplified store implementation for SwiftUI previews that combines
+/// both presenter and interactor functionality in a single object.
 @MainActor
-@Observable
-final class AnyPresenter<S: StoreState, Value> {
-    var value: Value
-    
-    init(initialValue: Value) {
-        self.value = initialValue
-    }
-}
-
-// Extension to make AnyPresenter work as Presenter for previews
-extension AnyPresenter {
-    static func preview(_ initialValue: Value) -> Presenter<S, Value> {
-        // Create a dummy stream that never emits
-        let stream = AsyncStream<S> { _ in }
-        
-        // This is a workaround - we create a real Presenter with a dummy keypath
-        // Since we can't create keypaths generically, we'll use a different approach
-        fatalError("Use mock store instead")
-    }
-}
-
-// Better approach: Create a minimal mock store for previews
-actor PreviewStore<S: StoreState, A: Action> {
-    var state: S
-    let reducer: Reducer<S, A>
-    
+final class MockStore<S: StoreState, A: Action>: Interacting {
+    private var state: S
+    private let reducer: Reducer<S, A>
     private var continuations: [AsyncStream<S>.Continuation] = []
     
     init(initialState: S, reducer: @escaping Reducer<S, A>) {
@@ -90,64 +70,62 @@ actor PreviewStore<S: StoreState, A: Action> {
         self.reducer = reducer
     }
     
-    func subscribe() -> AsyncStream<S> {
-        AsyncStream { continuation in
-            continuations.append(continuation)
-            continuation.yield(state)
+    // MARK: - Interacting Conformance
+    
+    nonisolated func send(_ action: A) async {
+        await MainActor.run {
+            print("Preview action: \(action)")
+            reducer(&state, action)
+            
+            // Notify all subscribers of the state change
+            for continuation in continuations {
+                continuation.yield(state)
+            }
         }
     }
     
-    func dispatch(_ action: A) {
-        reducer(&state, action)
-        for continuation in continuations {
-            continuation.yield(state)
+    // MARK: - Presenter Creation
+    
+    /// Creates a Presenter for a specific keypath of the state
+    func presenter<Value: Equatable>(
+        for keyPath: KeyPath<S, Value>
+    ) -> Presenter<S, Value> {
+        let stream = AsyncStream<S> { [weak self] continuation in
+            guard let self else {
+                continuation.finish()
+                return
+            }
+            self.continuations.append(continuation)
+            continuation.yield(self.state)
         }
+        
+        return Presenter(
+            initialValue: state[keyPath: keyPath],
+            stateStream: stream,
+            keyPath: keyPath
+        )
     }
 }
 
+// MARK: - Preview
+
 #Preview {
-    Group {
-        PreviewContent()
-    }
-    .frame(width: 500, height: 400)
-    .background(Color(.windowBackgroundColor))
+    PreviewContent()
+        .frame(width: 500, height: 400)
+        .background(Color(.windowBackgroundColor))
 }
 
 private struct PreviewContent: View {
-    @State private var presenter: Presenter<CounterState, Int>?
+    @State private var mockStore = MockStore(
+        initialState: CounterState(count: 42),
+        reducer: counterReducer
+    )
     
     var body: some View {
-        Group {
-            if let presenter {
-                CounterView(
-                    interactor: PreviewInteractor(),
-                    counterPresenter: presenter
-                )
-            } else {
-                ProgressView()
-            }
-        }
-        .task {
-            let store = PreviewStore(
-                initialState: CounterState(count: 42),
-                reducer: counterReducer
-            )
-            
-            let stream = await store.subscribe()
-            let initialState = await store.state
-            
-            presenter = Presenter(
-                initialValue: initialState.count,
-                stateStream: stream,
-                keyPath: \.count
-            )
-        }
-    }
-}
-
-private final class PreviewInteractor: Interacting, @unchecked Sendable {
-    func send(_ action: CounterAction) async {
-        print("Preview action: \(action)")
+        CounterView(
+            interactor: mockStore,
+            counterPresenter: mockStore.presenter(for: \.count)
+        )
     }
 }
 #endif
